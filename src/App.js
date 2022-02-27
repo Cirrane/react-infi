80116ee355e8725ed256b0eedc66eb79a85d8526
Cirrane <cathal.kirrane@outlook.com>
2022-02-27 21:06:07 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from 'react';
+import { useState, useRef} from 'react';
+import TextField from '@mui/material/TextField';
+import useBookSearch from './useBookSearch'
+import style from './style.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+  const [query, setQuery] = useState('')
+  const [pageNumber, setPageNumer] = useState(1)
+
+  function handleSearch(e) {
+    setQuery(e.target.value)
+    setPageNumer(1)
+  }
+
+  const {
+     books,
+     hasMore,
+     loading,
+     error
+  } = useBookSearch(query, pageNumber)
+
+return(
+  <>
+  <TextField value={query} onChange={handleSearch}
+    helperText="Enter Book Name"
+    id="demo-helper-text-aligned"
+    label="Book Name"
+  />
+  {books.map(book => {
+    return <div key={book}>{book}</div>
+  })}
+  <div className='loading'>{loading && 'Loading . . .'}</div>
+  <div className='error'>{error && 'Check Logs . . .'}</div>
+  </>
+);
 }
-
-export default App;
