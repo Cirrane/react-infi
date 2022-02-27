dba0d7c00c0e0963e55277a376a1273cd4b32b0f
Cirrane <cathal.kirrane@outlook.com>
2022-02-27 21:49:19 +0000

    display results, infinate load

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { useState, useRef} from 'react';
+import { useState, useRef, useCallback} from 'react';
 import TextField from '@mui/material/TextField';
 import useBookSearch from './useBookSearch'
 import style from './style.css'
@@ -8,17 +8,31 @@ export default function App() {
   const [query, setQuery] = useState('')
   const [pageNumber, setPageNumer] = useState(1)
 
+  const {
+    books,
+    hasMore,
+    loading,
+    error
+ } = useBookSearch(query, pageNumber)
+
+  const observer = useRef()
+
+  const lastBookElementRef = useCallback(node => {
+    if (loading) return 
+    if (observer.current) observer.current.disconnect()
+    observer.current = new IntersectionObserver(entries => {
+      if (entries[0].isIntersecting && hasMore){
+        setPageNumer(prevPageNumber => prevPageNumber + 1)
+      }
+    })
+    if (node) observer.current.observe(node)
+  }, [loading, hasMore])
+
   function handleSearch(e) {
     setQuery(e.target.value)
     setPageNumer(1)
   }
 
-  const {
-     books,
-     hasMore,
-     loading,
-     error
-  } = useBookSearch(query, pageNumber)
 
 return(
   <>
@@ -27,8 +41,13 @@ return(
     id="demo-helper-text-aligned"
     label="Book Name"
   />
-  {books.map(book => {
-    return <div key={book}>{book}</div>
+  <div className='text-result'>{books.filter(books => !books.complete).length} Results </div>
+  {books.map((book, index) => {
+    if (books.length === index + 1){
+      return <div ref={lastBookElementRef} key={book}>{book}</div>
+    } else {
+      return <div key={book}>{book}</div>
+    }
   })}
   <div className='loading'>{loading && 'Loading . . .'}</div>
   <div className='error'>{error && 'Check Logs . . .'}</div>
